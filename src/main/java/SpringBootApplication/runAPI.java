01c1d1be30dda6295dd4883cdb6c647565cea679
jojoEmo <coldfeet0816@gmail.com>
2020-12-13 21:09:00 +0900

    First commit

---
+++ src/main/java/SpringBootApplication/runAPI.java
@@ -0,0 +1,13 @@
+package SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class runAPI {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/commento/SpringBootApplication/SpringBootApplicatin.java
@@ -0,0 +1,13 @@
+package com.commento.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
